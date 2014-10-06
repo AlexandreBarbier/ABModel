ed9223dc26681c0429ff8a5b6d6e82aa1b080669
Alexandre barbier <alexandr.barbier@gmail.com>
2014-10-06 19:37:51 -0400

    Find a solution for nested properties

--- ABModel/ABModel/ABModel.swift
+++ ABModel/ABModel/ABModel.swift
@@ -2,39 +2,41 @@
 //  ABModel.swift
 //  ABModel
 //
-//  Created by Alexandre Barbier on 24/06/14.
+//  Created by Alexandre Barbier on 23/08/14.
 //  Copyright (c) 2014 abarbier. All rights reserved.
 //
 
 import UIKit
 
 /**
-* IMPROVEMENT : I have to find a way to parse custom typed array from this class in order to avoid overriding 
+* IMPROVEMENT : I have to find a way to parse custom typed array from this class in order to avoid overriding
 *               setValue(value: AnyObject!, forKey key: String!) in each subclass
 */
 
 
 /**
 *
-* You just need to inherit from this class to be able to create object from JSON 
+* You just need to inherit from this class to be able to create object from JSON
 * You have to name your properties like the JSON keys or override the method replaceKey to rename a JSON key
 *
 */
-class ABModel: NSObject, Printable {
+
+public class ABModel: NSObject, Printable {
     
-    override var description :String
-    {
+    public override var description :String
+        {
         get
         {
-            return "ABModel super class you should override this method"
+            
+            return "ABModel super class you should override this method in \(NSStringFromClass(self.dynamicType))"
         }
     }
     
-    init() {
+    public override init() {
         super.init()
     }
     
-    init(dictionary:Dictionary<String, AnyObject>) {
+    public required init(dictionary:Dictionary<String, AnyObject>) {
         super.init()
         var finalDictionnary = dictionary
         for (key, value : AnyObject) in dictionary {
@@ -42,7 +44,6 @@ class ABModel: NSObject, Printable {
                 var replacementKey = self.replaceKey(key)
                 if replacementKey.isEmpty {
                     finalDictionnary.removeAtIndex(finalDictionnary.indexForKey(key)!)
-                    
                 }
                 else {
                     finalDictionnary[replacementKey] = value;
@@ -54,17 +55,45 @@ class ABModel: NSObject, Printable {
         self.setValuesForKeysWithDictionary(finalDictionnary)
     }
     
-    override func setValue(value: AnyObject!, forKey key: String!)  {
+    override public func setValue(value: AnyObject!, forKey key: String!)  {
         
-        /**
-        * here we want to check the type of the property named key to know if it's an array / dictionnary.
-        * if it's an array / dictionnary we have to know the objects type contain in it to make something smart
-        * if we can't find a solution here we just have to override setValue in each subclass and apply the correct treatment 
-        * for nested properties
-        */
+
         //here we check if the value is nil to avoid crash
         if (value != nil) {
-            super.setValue(value, forKey: key)
+            /**
+            * here we want to check the type of the property named key to know if it's an array / dictionnary.
+            * for nested properties
+            */
+            if (value is [AnyObject] && value is Array<Dictionary<String, AnyObject>>) {
+                var arrayValue = self.valueForKey(key) as [ABModel]
+                if arrayValue.count > 0 {
+                    var arrayElementType = arrayValue[0].dynamicType
+                    arrayValue.removeAll(keepCapacity: false)
+                    for val in value as Array<Dictionary<String, AnyObject>> {
+                        var l = arrayElementType(dictionary: val)
+                        arrayValue.append(l)
+                    }
+                    super.setValue(arrayValue, forKey: key)
+                    
+                }
+                else {
+                    println("\n#### FATAL ERROR ####\n key : \(key) is not initialised like this [CUSTOM_TYPE()]")
+                    fatalError("Error in parsing see console for more information")
+                    
+                }
+                
+            }
+            else if (value is Dictionary<String, AnyObject> && !(self.valueForKey(key) is Dictionary<String, AnyObject>)) {
+                var arrayValue = self.valueForKey(key) as ABModel
+                var t = arrayValue.dynamicType
+                var val = value as Dictionary<String, AnyObject>
+                
+                arrayValue = t(dictionary: val)
+                super.setValue(arrayValue, forKey: key)
+            }
+            else {
+                super.setValue(value, forKey: key)
+            }
         }
         
     }
@@ -72,7 +101,8 @@ class ABModel: NSObject, Printable {
     /**
     * You should override this method only if you want to rename JSON key
     */
-    func replaceKey(key:String) -> String {
+    public func replaceKey(key:String) -> String {
+        
         return "";
     }
 }
