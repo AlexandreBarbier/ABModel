7f08d3cf9ef42fd28ffea43b2d1150db75e59326
Alexandre barbier <alexandr.barbier@gmail.com>
2016-11-09 16:32:49 +0100

    add test

--- ABModel/ABModel.swift
+++ ABModel/ABModel.swift
@@ -139,8 +139,8 @@ extension ABModel {
         let objectValue = self.value(forKey: key)
         if (value is [AnyObject] && value is Array<Dictionary<String, AnyObject>>) {
             let k = class_getInstanceVariable(type(of:self), key)
-            let t = Reflection().getTypeOf(k)!
-            print(t)
+            let t = Reflection().getTypeOf(k)
+            print(t ?? "test")
             guard var newArray = objectValue as? [ABModel] , newArray.count > 0 else {
                 print("\n#### FATAL ERROR ####\n key : \(key) is not initialised like this [CUSTOM_TYPE()] in \(NSStringFromClass(type(of: self)))")
                 fatalError("Error in parsing see console for more information")
